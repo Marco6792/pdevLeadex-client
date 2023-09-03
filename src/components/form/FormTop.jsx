88dc90d3240a23dad64992b07d74241c75b39501
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-03 07:30:15 +0100

    tt

--- src/components/form/FormTop.jsx
+++ src/components/form/FormTop.jsx
@@ -5,7 +5,7 @@ import Bounce from "react-reveal/Bounce"
 const FormTop = () => {
   return (
     <div className="flex flex-col justify-center md:h-[450px] w-full items-center max-sm:h-full ">
-    <div className="relative overflow-hidden w-full h-full   flex flex-col md:w-[500px]">
+    <div className="relative  w-full h-full   flex flex-col md:w-[500px]">
 
     <Bounce right><TopDesign /></Bounce>
       </div>
