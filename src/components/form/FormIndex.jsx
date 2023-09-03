b77d9c8cd3a5625ca1a939a200076a5726bd8337
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-03 07:54:48 +0100

    lllll

--- src/components/form/FormIndex.jsx
+++ src/components/form/FormIndex.jsx
@@ -5,7 +5,7 @@ import FormTop from './FormTop'
 const FormIndex = () => {
   return (
     <div className="flex flex-col z-20 fixed top-0 bg-white justify-center h-screen w-full items-center max-sm:h-screen">
-    <div className="relative w-full max-md:h-screen h-full md:h-[950px] md:shadow-md overflow-scroll scrollbar-hide bg-black md:rounded-[19px] flex flex-col md:w-[500px]">
+    <div className="relative w-full max-md:h-screen h-full md:h-[950px] md:shadow-md overflow-y-scrol-scroll scrollbar-hide md:rounded-[19px] flex flex-col md:w-[500px]">
      <FormTop />
       <Outlet />
       </div>
