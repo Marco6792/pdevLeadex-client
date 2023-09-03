3d80d33cc7ef564c454f9b5e2db6d22ede506fd3
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-03 08:25:25 +0100

    ooo

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -48,8 +48,8 @@ const NavBar = () => {
   const { username } = userInfo;
 
   return (
-    <div className="top-0 w-full py-6 z-10 fixed shadow-md bg-white bg-red-300">
-      <div className="mx-auto max-xms:w-screen max-md:w-[90.777%] max-md:px-3 w-5/6 flex justify-between items-center gap-10 max-md:gap-3 overflow-x-hidden bg-black">
+    <div className="top-0 w-full py-6 z-10 fixed shadow-sm">
+      <div className="mx-auto max-xms:w-full max-md:w-[90.777%] max-md:px-3 w-5/6 flex justify-between items-center gap-10 max-md:gap-3 bg-black">
         {/*left side Logo */}
         <Link to="/">
           <div className="flex items-center space-x-1 cursor-pointer  max-xms:-ml-2">
