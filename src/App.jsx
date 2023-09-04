445d4945d65fa3a429904ba50897acad156082bb
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-04 21:47:38 +0100

    dasda

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import OnlineCheck from "./scene/OnlineCheck";
 import BooksPage from "./pages/BooksPage";
-import SearchPage from "./pages/SearchPage/SearchPage";
+import SearchPage from "./pages/searchPage/SearchPage";
 import StudyPage from "./pages/StudyPage";
 import PapersPage from "./pages/PapersPage";
 import QuizzePage from "./pages/QuizzePage";
