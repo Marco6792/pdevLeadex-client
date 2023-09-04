efc28d24b3edda247495f1433f443784155cd0cc
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-04 21:50:55 +0100

    dsadsad

--- src/pages/searchPage/SearchPage.jsx
+++ src/pages/searchPage/SearchPage.jsx
@@ -7,7 +7,7 @@ const SearchPage = () => {
   return (
     <div className="w-screen h-screen mt-20">
     <SearchBar />
-    <h1>Search page</h1>
+    <h1>Search changes</h1>
     <ScrollToTopButton />
     <FixedBottomMenu />
   </div>
