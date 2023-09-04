a543b3e0445e5faefe5dc2b5ae32a8886fe20332
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-04 23:34:29 +0100

    dsadad

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
 
   return (
     <div className="hero text-white py-20 mt-20 px-3">
-    <h1 className="text-4xl font-bold mb-4">Welcome to the Hero Page</h1>
+    <h1 className="text-4xl font-bold mb-4">Welcome to my the Hero Page</h1>
     <p className="text-lg mb-6">This is the main section of your website.</p>
     <button className="bg-white text-blue-500 py-2 px-4 rounded-lg shadow-md hover:bg-blue-200"
     onClick={handleClick}
