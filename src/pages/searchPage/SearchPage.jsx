9a886ebacb7f2e9b7697e79c45ed2377b6c9db68
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-04 21:53:30 +0100

    gdfgdfg

--- src/pages/searchPage/SearchPage.jsx
+++ src/pages/searchPage/SearchPage.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
 import ScrollToTopButton from "../../scene/ScrollToTopButton";
 import FixedBottomMenu from "../../components/sections/FixedBottomMenu";
-import SearchBar from './searchBar';
+// import SearchBar from './searchBar';
 
 const SearchPage = () => {
   return (
     <div className="w-screen h-screen mt-20">
-    <SearchBar />
+    {/* <SearchBar /> */}
     <h1>Search changes</h1>
     <ScrollToTopButton />
     <FixedBottomMenu />
