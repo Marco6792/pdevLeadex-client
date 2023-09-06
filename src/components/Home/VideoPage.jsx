5bb8f99b80eabc3081db3eeaccc848ce4b813b31
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-07 00:19:33 +0100

    czczxcxzcxzc

--- src/components/Home/VideoPage.jsx
+++ src/components/Home/VideoPage.jsx
@@ -6,7 +6,7 @@ const VideoPage = () => {
   return (
     <div className="grid  grid-cols-2 mt-[100rem] justify-center items-center gap-x-10 gap-y-6 xl:max-w-6xl mx-auto max-xl:grid-cols-1 px-4">
         <div className="flex border">
-        <div style="padding:56.34% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/861833073?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="10000000_496829205436638_241453143440490665_n"></iframe></div>
+      
         </div>
     </div>
   );
