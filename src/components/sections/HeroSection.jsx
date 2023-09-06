13b55ea46cf802342bc50a6097f12d334334da9e
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-07 00:33:37 +0100

    asdsad

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
       <div className=" gradient bg-blur text-white py-20 mt-20 px-10 opacity-90">
         <h1 className="text-4xl font-bold mb-4">Welcome to the Hero Page</h1>
         <p className="text-lg mb-6">
-          This is the main section of your website.
+          some stuff to be added here!.
         </p>
         <button className="bg-white text-blue-500 py-2 px-9 rounded-lg shadow-md hover:bg-blue-200">
           Get Started
