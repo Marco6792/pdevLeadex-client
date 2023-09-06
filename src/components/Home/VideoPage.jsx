b8d379620e2e4c48907531423256ff4ae6caafaa
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-09-06 23:39:43 +0100

    ddddddddddddddddddddd

--- src/components/Home/VideoPage.jsx
+++ src/components/Home/VideoPage.jsx
@@ -5,11 +5,18 @@ const VideoPage = () => {
   const url = ["https://vimeo.com/860706802?share=copy"];
   return (
     <div className="grid  grid-cols-2 mt-[100rem] justify-center items-center gap-x-10 gap-y-6 xl:max-w-6xl mx-auto max-xl:grid-cols-1 px-4">
-        <div className="flex bg-yellow-400">
-        <ReactPlayer url={url} className="video" controls />
-        </div>
-        <div className="flex">
-        <ReactPlayer url={url} className="video" controls />
+        <div className="flex border">
+        <ReactPlayer
+        //url="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
+        // url="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
+        url="https://vimeo.com/860706802"
+        playing={false}
+        controls={true}
+        // volume={null}
+        // muted={false}
+        //width="100%"
+        //height="100%"
+      />
         </div>
     </div>
   );
